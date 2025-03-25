8e453c6162b7813cd43ad57d1606b137029e3f37
Ádám <sziver.adi@gmail.com>
2025-03-25 20:40:04 +0100

    idk

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import request
 import time
 
 
-async def send_request(video_name):
-    await request.post(video_name)  # Ensure this is an async function
+# async def send_request(video_name):
+#     await request.post(video_name)  # Ensure this is an async function
 
 
 async def main():
@@ -22,18 +22,19 @@ async def main():
     frame_rate = 14
 
     folder_path = Path("sightings")
-    tasks = []  # Store tasks
+    # tasks = []  # Store tasks
 
     for folder in folder_path.iterdir():
         if folder.is_dir():
             folder_name = f"sightings/{folder.name}"
             video_name = videoMaker.make_video(folder_name, f"{folder_name}/{output_video}", frame_rate)
 
-            # Schedule the request task
-            task = asyncio.create_task(send_request(video_name))
-            tasks.append(task)
+            await request.post(video_name)
+            # # Schedule the request task
+            # task = asyncio.create_task(send_request(video_name))
+            # tasks.append(task)
 
-    await asyncio.gather(*tasks)  # Wait for all requests to finish
+    # await asyncio.gather(*tasks)  # Wait for all requests to finish
 
     elapsed_time = time.time() - start_time
     print(f"Elapsed time: {elapsed_time:.2f} seconds")
--- request.py
+++ request.py
@@ -20,7 +20,6 @@ async def post(path):
 
     async with httpx.AsyncClient() as client:
         # response = await client.post(url, data=data, auth=(username, password))
-        print(url)
         response = await client.post(url, data=data)
 
     if response.status_code == 200:
@@ -28,3 +27,32 @@ async def post(path):
         print(response.json())  # or response.text for raw response
     else:
         print("Request failed with status code:", response.status_code)
+
+# import requests
+# from requests.auth import HTTPBasicAuth
+# import configparser
+#
+# def post(path):
+#
+#     config = configparser.ConfigParser()
+#     config.read("config.ini")
+#
+#     username = config["request"]["username"]
+#     password = config["request"]["password"]
+#     # Open the video file in binary mode
+#     with open(path, "rb") as video_file:
+#         data = {
+#             "video": video_file,
+#             "username": username,
+#             "password": password
+#         }
+#
+#         # Make the POST request with Basic Authentication
+#         response = requests.post(config["request"]["url"], data=data)
+#
+#     # Check the response status
+#     if response.status_code == 200:
+#         print("Request successful!")
+#         print(response.json())  # or response.text to print raw response
+#     else:
+#         print("Request failed with status code:", response.status_code)
